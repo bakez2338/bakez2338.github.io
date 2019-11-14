06a47cf0f0803cf100bdcf50d1db55bc15f53b48
bakez2338 <54628748+bakez2338@users.noreply.github.com>
2019-11-14 09:55:02 -0500

    Add files via upload

--- BPA Judged Website/src/js/sidebar.js
+++ BPA Judged Website/src/js/sidebar.js
@@ -1,4 +1,5 @@
 function toggleMenu(element) {
     element.classList.toggle("menu-open");
     document.getElementById("sidebar").classList.toggle("show-sidebar");
+    document.getElementById("sidebar-cover").classList.toggle("show-sidebar");
 }
