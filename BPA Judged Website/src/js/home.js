a3abddd93b07f1c2f6c0847d004eb9f9719cec96
unknown <bakez2338@student.oregoncs.org>
2020-01-16 10:18:27 -0500

    Update

--- BPA Judged Website/src/js/home.js
+++ BPA Judged Website/src/js/home.js
@@ -3,9 +3,9 @@ var lw = 0;
 function tick() {
     if (lh != screen.availHeight || lw != screen.availWidth) {
         if (screen.availHeight < screen.availWidth) {
-            document.getElementById("hometable").innerHTML = '<tbody><tr><!--Columns of the tables each containing a div with an image as the background, as well as some text to put on top of the image--><td><a class="divimagelink" href="./Green/index.html"><div id="green"><strong>The Ways We\'re Green</strong></div></a></td><td><a class="divimagelink" href="./Homes/index.html"><div id="homes"><strong>Find Homes</strong></div></a></td><td><a class="divimagelink" href="./About/index.html"><div id="about"><strong>Learn More About Us</strong></div></a></td></tr></tbody>'
+            document.getElementById("hometable").innerHTML = '<tbody class="landscape"><tr><!--Columns of the tables each containing a div with an image as the background, as well as some text to put on top of the image--><td><a class="divimagelink" href="./Green/index.html"><div id="green"><strong>The Ways We\'re Green</strong></div></a></td><td><a class="divimagelink" href="./Homes/index.html"><div id="homes"><strong>Find Homes</strong></div></a></td><td><a class="divimagelink" href="./About/index.html"><div id="about"><strong>Learn More About Us</strong></div></a></td></tr></tbody>'
         } else {
-            document.getElementById("hometable").innerHTML = '<tbody><tr><td><a class="divimagelink" href="./Green/index.html"><div id="green"><strong>The Ways We\'re Green</strong></div></a></td></tr><tr><td><a class="divimagelink" href="./Homes/index.html"><div id="homes"><strong>Find Homes</strong></div></a></td></tr><tr><td><a class="divimagelink" href="./About/index.html"><div id="about"><strong>Learn More About Us</strong></div></a></td></tr></tbody>';
+            document.getElementById("hometable").innerHTML = '<tbody class="portrait"><tr><td><a class="divimagelink" href="./Green/index.html"><div id="green"><strong>The Ways We\'re Green</strong></div></a></td></tr><tr><td><a class="divimagelink" href="./Homes/index.html"><div id="homes"><strong>Find Homes</strong></div></a></td></tr><tr><td><a class="divimagelink" href="./About/index.html"><div id="about"><strong>Learn More About Us</strong></div></a></td></tr></tbody>';
         }
         lh = screen.availHeight;
         lw = screen.availWidth;
